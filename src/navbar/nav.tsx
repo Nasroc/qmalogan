af00e521135c19acd68a2bc27fb7e21a408eeaf2
Carson Benedetto <154766384+Nasroc@users.noreply.github.com>
2025-03-03 13:36:41 -0700

    fixed nav menu

--- src/navbar/nav.tsx
+++ src/navbar/nav.tsx
@@ -1,4 +1,6 @@
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { Disclosure, DisclosureButton, DisclosurePanel,
+  
+} from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import logo from "./qma-logo.png";
 
