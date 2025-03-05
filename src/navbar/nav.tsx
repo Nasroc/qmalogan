50e2a9454594ffa7b2bda8c7b945e028596ffba9
Carson Benedetto <154766384+Nasroc@users.noreply.github.com>
2025-03-04 22:26:44 -0700

    fix

--- src/navbar/nav.tsx
+++ src/navbar/nav.tsx
@@ -51,7 +51,6 @@ const NavBar: React.FC<NavBarProps> = ({ navigation, setNavigation }) => {
                 {navigation.map((item) => (
                   <a
                     key={item.name}
-                    href="#"
                     className={classNames(
                       item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'px-3 py-2 rounded-md text-sm font-medium'
@@ -84,7 +83,6 @@ const NavBar: React.FC<NavBarProps> = ({ navigation, setNavigation }) => {
             <Disclosure.Button
               key={item.name}
               as="a"
-              href={item.href}
               aria-current={item.current ? 'page' : undefined}
               className={classNames(
                 item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
