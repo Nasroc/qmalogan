73bbc55062b3c80fa6e74edde57a73b247079184
Carson Benedetto <154766384+Nasroc@users.noreply.github.com>
2025-03-04 22:22:16 -0700

    fix

--- src/navbar/nav.tsx
+++ src/navbar/nav.tsx
@@ -51,7 +51,7 @@ const NavBar: React.FC<NavBarProps> = ({ navigation, setNavigation }) => {
                 {navigation.map((item) => (
                   <a
                     key={item.name}
-                    href={item.href}
+                    href="#"
                     className={classNames(
                       item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'px-3 py-2 rounded-md text-sm font-medium'
