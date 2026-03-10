c03dd77556cbb9765c6dca17e88c5ebbbf145ed2
Dean Lanier <dean.lanier@gmail.com>
2026-03-09 23:14:59 -0600

    Add contact section

--- src/Public/Dashboard/dashboard.tsx
+++ src/Public/Dashboard/dashboard.tsx
@@ -1,11 +1,13 @@
 import { AboutUs } from "./aboutUs";
+import { ContactUs } from "./contactUs";
 import { FAQ } from "./FAQ";
 
 export function Dashboard() {
   return (
-    <div className="custom-bg custom-dark transition-colors duration-300" style= {{minHeight: 'calc(100vh - 5rem)'}}>
+    <div className="custom-bg-light dark:custom-bg custom-dark transition-colors duration-300" style= {{minHeight: 'calc(100vh - 5rem)'}}>
       <AboutUs />
       <FAQ />
+      <ContactUs />
     </div>
   );
 }
